1a2bdeeff807e796fc79139a5f81e04ea8ef0808
uvsq22007820 <72186096+uvsq22007820@users.noreply.github.com>
2021-04-12 15:06:20 +0200

    Canvas

--- Projet_Tapatan_groupe3.py
+++ Projet_Tapatan_groupe3.py
@@ -8,3 +8,12 @@
 # https://github.com/uvsq22008953/Projet_Tapatan
 #########################################
 
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Jeu du Tapatan")
+canvas = tk.Canvas(root, width=1200, height=1200, borderwidth=0, highlightthickness=0, bg="grey")
+canvas.pack(padx =1, pady =1)
+canvas.grid()
+root.mainloop()
+	
