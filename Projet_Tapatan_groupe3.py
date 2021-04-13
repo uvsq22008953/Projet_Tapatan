31d1235329da7e2522d2e64f4763fdfab06e22c7
uvsq22007820 <72186096+uvsq22007820@users.noreply.github.com>
2021-04-13 22:50:09 +0200

    Modif taille canvas

--- Projet_Tapatan_groupe3.py
+++ Projet_Tapatan_groupe3.py
@@ -1,49 +1,49 @@
-#########################################
-# groupe MPCI 3
-# Bertuit Marlone
-# Moreira Théo  testdvx
-# Lopes Ferreira Lucas 
-#Baali Wassim 
-#Fernandez Sébastien 
-# https://github.com/uvsq22008953/Projet_Tapatan
-#########################################
+""""
+groupe MPCI 3
+Bertuit Marlone
+Moreira Théo  testdvx
+Lopes Ferreira Lucas 
+Baali Wassim 
+Fernandez Sébastien 
+https://github.com/uvsq22008953/Projet_Tapatan
+"""
 
 import tkinter as tk
 
 def ClicCase(event):
     #print('coordonnées', event.x, event.y)
-    if event.x>=50 and event.x<=150 and event.y>=50 and event.y<=150:
+    if event.x>=25 and event.x<=75 and event.y>=25 and event.y<=75:
         print('case1')
-    elif event.x>=550 and event.x<=650 and event.y>=50 and event.y<=150:
+    elif event.x>=375 and event.x<=425 and event.y>=25 and event.y<=75:
         print('case2')
-    elif event.x>=1050 and event.x<=1150 and event.y>=50 and event.y<=150:
+    elif event.x>=725 and event.x<=775 and event.y>=25 and event.y<=75:
         print('case3')
-    elif event.x>=50 and event.x<=150 and event.y>=550 and event.y<=650:
+    elif event.x>=25 and event.x<=75 and event.y>=375 and event.y<=425:
         print('case4')
-    elif event.x>=550 and event.x<=650 and event.y>=550 and event.y<=650:
+    elif event.x>=375 and event.x<=425 and event.y>=375 and event.y<=425:
         print('case5')
-    elif event.x>=1050 and event.x<=1150 and event.y>=550 and event.y<=650:
+    elif event.x>=725 and event.x<=775 and event.y>=375 and event.y<=425:
         print('case6')
-    elif event.x>=50 and event.x<=150 and event.y>=1050 and event.y<=1150:
+    elif event.x>=25 and event.x<=75 and event.y>=725 and event.y<=775:
         print('case7')
-    elif event.x>=550 and event.x<=650 and event.y>=1050 and event.y<=1150:
+    elif event.x>=375 and event.x<=425 and event.y>=725 and event.y<=775:
         print('case8')
-    elif event.x>=1050 and event.x<=1150 and event.y>=1050 and event.y<=1150:
+    elif event.x>=725 and event.x<=775 and event.y>=725 and event.y<=775:
         print('case9')
 
 root = tk.Tk()
 root.title("Jeu du Tapatan")
-canvas = tk.Canvas(root, width=1200, height=1200, borderwidth=0, highlightthickness=0, bg="grey")
+canvas = tk.Canvas(root, width=800, height=800, borderwidth=0, highlightthickness=0, bg="grey")
 canvas.pack(padx =1, pady =1)
 canvas.grid()
 canvas.bind('<ButtonPress-1>', ClicCase)
-ligne1 = canvas.create_line(100, 100, 1100, 1100)
-ligne2 = canvas.create_line(600, 100, 600, 1100)
-ligne3 = canvas.create_line(1100, 100, 100, 1100)
-ligne4 = canvas.create_line(100, 600, 1100, 600)
-ligneh1 = canvas.create_line(100, 100, 1100, 100)
-ligneh2 = canvas.create_line(100, 1100, 1100, 1100)
-lignev1 = canvas.create_line(100, 100, 100, 1100)
-lignev2 = canvas.create_line(1100, 100, 1100, 1100)
+ligne1 = canvas.create_line(50, 50, 750, 750)
+ligne2 = canvas.create_line(400, 50, 400, 750)
+ligne3 = canvas.create_line(750, 50, 50, 750)
+ligne4 = canvas.create_line(50, 400, 750, 400)
+ligneh1 = canvas.create_line(50, 50, 750, 50)
+ligneh2 = canvas.create_line(50, 750, 750, 750)
+lignev1 = canvas.create_line(50, 50, 50, 750)
+lignev2 = canvas.create_line(750, 50, 750, 750)
 
 root.mainloop()
