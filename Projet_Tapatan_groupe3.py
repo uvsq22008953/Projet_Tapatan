4387dd3e89a1d7ab0c5a07a27d34e2e271d1e1be
uvsq22007820 <72186096+uvsq22007820@users.noreply.github.com>
2021-04-13 23:05:53 +0200

    placements pions

--- Projet_Tapatan_groupe3.py
+++ Projet_Tapatan_groupe3.py
@@ -9,6 +9,7 @@ https://github.com/uvsq22008953/Projet_Tapatan
 """
 
 import tkinter as tk
+import random
 
 def ClicCase(event):
     #print('coordonnées', event.x, event.y)
@@ -31,6 +32,7 @@ def ClicCase(event):
     elif event.x>=725 and event.x<=775 and event.y>=725 and event.y<=775:
         print('case9')
 
+
 root = tk.Tk()
 root.title("Jeu du Tapatan")
 canvas = tk.Canvas(root, width=800, height=800, borderwidth=0, highlightthickness=0, bg="grey")
@@ -45,5 +47,59 @@ ligneh1 = canvas.create_line(50, 50, 750, 50)
 ligneh2 = canvas.create_line(50, 750, 750, 750)
 lignev1 = canvas.create_line(50, 50, 50, 750)
 lignev2 = canvas.create_line(750, 50, 750, 750)
+
+def _create_circle(x, y, x2, y2, **kwargs):
+    return canvas.create_oval(x, y, x2, y2, **kwargs)
+tk.Canvas.create_circle = _create_circle
+
+#VARIABLES POSITIONS PIONS
+x1b1=0
+x2b1=0
+y1b1=0
+y2b1=0
+
+x1b2=0
+x2b2=0
+y1b2=0
+y2b2=0
+
+x1b3=0
+x2b3=0
+y1b3=0
+y2b3=0
+
+x1r1=0
+x2r1=0
+y1r1=0
+y2r1=0
+
+x1r2=0
+x2r2=0
+y1r2=0
+y2r2=0
+
+x1r3=0
+x2r3=0
+y1r3=0
+y2r3=0
+
+premier_tour=random.randint(0,2)
+
+if premier_tour==0:
+    premier=0
+    #ROUGE COMMENCENT
+else:
+    premier=1
+    #BLEUS COMMENCENT
+
+pion_b_1= _create_circle(x1b1, x2b1, y1b1, y2b2, fill="blue", width=2)
+pion_b_2= _create_circle(x1b2, x1b2, x1b2, x1b2, fill="blue", width=2)
+pion_b_3= _create_circle(x1b3, x1b3, x1b3, x1b3, fill="blue", width=2)
+pion_r_1= _create_circle(x1r1, x1r1, x1r1, x1r1, fill="red", width=2)
+pion_r_2= _create_circle(x1r2, x1r2, x1r2, x1r2, fill="red", width=2)
+pion_r_3= _create_circle(x1r3, x1r3, x1r3, x1r3, fill="red", width=2)
+
+
+
 
 root.mainloop()
