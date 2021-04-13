dfdf4ea90558c3d5d4e99dea2c0f1902d4615b6e
uvsq22007820 <72186096+uvsq22007820@users.noreply.github.com>
2021-04-13 23:21:30 +0200

    déclaration variables

--- Projet_Tapatan_groupe3.py
+++ Projet_Tapatan_groupe3.py
@@ -11,6 +11,66 @@ https://github.com/uvsq22008953/Projet_Tapatan
 import tkinter as tk
 import random
 
+#VARIABLES POSITIONS PIONS
+x1b1=0
+x2b1=0
+y1b1=0
+y2b1=0
+"""""""
+x1b2=0
+x2b2=0
+y1b2=0
+y2b2=0
+""""""
+x1b3=0
+x2b3=0
+y1b3=0
+y2b3=0
+""""""
+x1r1=0
+x2r1=0
+y1r1=0
+y2r1=0
+""""""
+x1r2=0
+x2r2=0
+y1r2=0
+y2r2=0
+""""""
+x1r3=0
+x2r3=0
+y1r3=0
+y2r3=0
+""""""
+#VALEURS QUE L'ON VA MODIFIER
+x1=0
+x2=0
+y1=0
+y2=0
+""""""
+nb_tours=0
+tourdejouer=0
+# 0 si tour des rouges // 1 si tour des bleus
+premier_tour=random.randint(0,2)
+if premier_tour==0:
+    tourdejouer=0
+    #ROUGES COMMENCENT
+else:
+    tourdejouer=1
+    #BLEUS COMMENCENT
+nb_placements_bleu=0
+nb_placements_rouge=0
+case1_libre=True
+case2_libre=True
+case3_libre=True
+case4_libre=True
+case5_libre=True
+case6_libre=True
+case7_libre=True
+case8_libre=True
+case9_libre=True
+
+#FONCTIONS
 def ClicCase(event):
     #print('coordonnées', event.x, event.y)
     if event.x>=25 and event.x<=75 and event.y>=25 and event.y<=75:
@@ -32,7 +92,6 @@ def ClicCase(event):
     elif event.x>=725 and event.x<=775 and event.y>=725 and event.y<=775:
         print('case9')
 
-
 root = tk.Tk()
 root.title("Jeu du Tapatan")
 canvas = tk.Canvas(root, width=800, height=800, borderwidth=0, highlightthickness=0, bg="grey")
@@ -52,52 +111,13 @@ def _create_circle(x, y, x2, y2, **kwargs):
     return canvas.create_oval(x, y, x2, y2, **kwargs)
 tk.Canvas.create_circle = _create_circle
 
-#VARIABLES POSITIONS PIONS
-x1b1=0
-x2b1=0
-y1b1=0
-y2b1=0
-
-x1b2=0
-x2b2=0
-y1b2=0
-y2b2=0
-
-x1b3=0
-x2b3=0
-y1b3=0
-y2b3=0
-
-x1r1=0
-x2r1=0
-y1r1=0
-y2r1=0
-
-x1r2=0
-x2r2=0
-y1r2=0
-y2r2=0
-
-x1r3=0
-x2r3=0
-y1r3=0
-y2r3=0
-
-premier_tour=random.randint(0,2)
-
-if premier_tour==0:
-    premier=0
-    #ROUGE COMMENCENT
-else:
-    premier=1
-    #BLEUS COMMENCENT
-
 pion_b_1= _create_circle(x1b1, x2b1, y1b1, y2b2, fill="blue", width=2)
 pion_b_2= _create_circle(x1b2, x1b2, x1b2, x1b2, fill="blue", width=2)
 pion_b_3= _create_circle(x1b3, x1b3, x1b3, x1b3, fill="blue", width=2)
 pion_r_1= _create_circle(x1r1, x1r1, x1r1, x1r1, fill="red", width=2)
 pion_r_2= _create_circle(x1r2, x1r2, x1r2, x1r2, fill="red", width=2)
 pion_r_3= _create_circle(x1r3, x1r3, x1r3, x1r3, fill="red", width=2)
+
 
 
 
