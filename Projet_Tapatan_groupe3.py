bf8b1ac69a6057e1b12e7ea9b7b7162d2cc41264
uvsq22007820 <72186096+uvsq22007820@users.noreply.github.com>
2021-04-14 02:29:25 +0200

    Ajouter pion 6 premiers tours (case 9 uniquement)

--- Projet_Tapatan_groupe3.py
+++ Projet_Tapatan_groupe3.py
@@ -48,16 +48,11 @@ x2=0
 y1=0
 y2=0
 """"""
-nb_tours=0
-tourdejouer=0
+nb_tours=1
 # 0 si tour des rouges // 1 si tour des bleus
-premier_tour=random.randint(0,2)
-if premier_tour==0:
-    tourdejouer=0
-    #ROUGES COMMENCENT
-else:
-    tourdejouer=1
-    #BLEUS COMMENCENT
+tourdejouer=random.randint(0, 1)
+print(tourdejouer)
+phase_placement=1
 nb_placements_bleu=0
 nb_placements_rouge=0
 case1_libre=True
@@ -69,55 +64,156 @@ case6_libre=True
 case7_libre=True
 case8_libre=True
 case9_libre=True
+selectionner=0
 
 #FONCTIONS
 def ClicCase(event):
     #print('coordonnées', event.x, event.y)
+    global tourdejouer
+    global nb_placements_bleu
+    global nb_placements_rouge
+    global nb_tours
     if event.x>=25 and event.x<=75 and event.y>=25 and event.y<=75:
         #print('case1')
-        canvas.coords(pion_selectionne, 25, 25, 75, 75)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 25, 25, 75, 75)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=375 and event.x<=425 and event.y>=25 and event.y<=75:
         #print('case2')
-        canvas.coords(pion_selectionne, 375, 25, 425, 75)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 375, 25, 425, 75)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=725 and event.x<=775 and event.y>=25 and event.y<=75:
         #print('case3')
-        canvas.coords(pion_selectionne, 725, 25, 775, 75)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 725, 25, 775, 75)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=25 and event.x<=75 and event.y>=375 and event.y<=425:
         #print('case4')
-        canvas.coords(pion_selectionne, 25, 375, 75, 425)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 25, 375, 75, 425)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=375 and event.x<=425 and event.y>=375 and event.y<=425:
         #print('case5')
-        canvas.coords(pion_selectionne, 375, 375, 425, 425)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 375, 375, 425, 425)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=725 and event.x<=775 and event.y>=375 and event.y<=425:
         #print('case6')
-        canvas.coords(pion_selectionne, 725, 375, 775, 425)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 725, 375, 775, 425)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=25 and event.x<=75 and event.y>=725 and event.y<=775:
         #print('case7')
-        canvas.coords(pion_selectionne, 25, 725, 75, 775)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 25, 725, 75, 775)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=375 and event.x<=425 and event.y>=725 and event.y<=775:
         #print('case8')
-        canvas.coords(pion_selectionne, 375, 725, 425, 775)
-        canvas.update()
-        selectionner=0
+        if selectionner!=0:
+            canvas.coords(pion_selectionne, 375, 725, 425, 775)
+            canvas.update()
+            if tourdejouer==0:
+                tourdejouer=1
+            elif tourdejouer==1:
+                tourdejouer=0
     elif event.x>=725 and event.x<=775 and event.y>=725 and event.y<=775:
         #print('case9')
-        canvas.coords(pion_selectionne, 725, 725, 775, 775)
-        canvas.update()
-        selectionner=0
+        if nb_tours>6:
+            if selectionner!=0:
+               canvas.coords(pion_selectionne, 725, 725, 775, 775)
+               canvas.update()
+               if tourdejouer==0:
+                    tourdejouer=1
+               elif tourdejouer==1:
+                    tourdejouer=0
+        else:
+            if tourdejouer==1:
+                if nb_placements_bleu==0:
+                    x1b1=725
+                    x2b1=725
+                    y1b1=775
+                    y2b1=775
+                    pion_b_1= canvas.create_rectangle(x1b1, x2b1, y1b1, y2b1, fill="blue", width=2)
+                    nb_placements_bleu+=1
+                    tourdejouer==0
+                    nb_tours+=1
+                if nb_placements_bleu==1:
+                    x1b2=725
+                    x2b2=725
+                    y1b2=775
+                    y2b2=775
+                    pion_b_2= canvas.create_rectangle(x1b2, x2b2, y1b2, y2b2, fill="blue", width=2)
+                    nb_placements_bleu+=1
+                    tourdejouer==0
+                    nb_tours+=1
+                if nb_placements_bleu==2:
+                    x1b3=725
+                    x2b3=725
+                    y1b3=775
+                    y2b3=775
+                    pion_b_3= canvas.create_rectangle(x1b3, x2b3, y1b3, y2b3, fill="blue", width=2)
+                    nb_placements_bleu+=1
+                    tourdejouer==0
+                    nb_tours+=1
+            elif tourdejouer==0:
+                if nb_placements_rouge==0:
+                    x1r1=725
+                    x2r1=725
+                    y1r1=775
+                    y2r1=775
+                    pion_r_1= canvas.create_rectangle(x1r1, x2r1, y1r1, y2r1, fill="red", width=2)
+                    nb_placements_rouge+=1
+                    tourdejouer==1
+                    nb_tours+=1
+                if nb_placements_rouge==1:
+                    x1r2=725
+                    x2r2=725
+                    y1r2=775
+                    y2r2=775
+                    pion_r_2= canvas.create_rectangle(x1r2, x2r2, y1r2, y2r2, fill="red", width=2)
+                    nb_placements_rouge+=1
+                    tourdejouer==1
+                    nb_tours+=1
+                if nb_placements_rouge==2:
+                    x1r3=725
+                    x2r3=725
+                    y1r3=775
+                    y2r3=775
+                    pion_r_3= canvas.create_rectangle(x1r3, x2r3, y1r3, y2r3, fill="red", width=2)
+                    nb_placements_rouge+=1
+                    tourdejouer==1
+                    nb_tours+=1
+
+        
 
 root = tk.Tk()
 root.title("Jeu du Tapatan")
